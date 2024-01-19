836d95c709140340eb8eb5b072eb6db6db06bd8b
Mathis Santos <mail@mathissantos.com>
2024-01-19 11:36:27 +0100

    imported image to pages/index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import Image from "next/image";
+import Whirlpool from "../public/Whirlpool.jpg";
 
 const StyledHeader = styled.h1`
   color: var(--link-color);
@@ -11,10 +13,17 @@ const StyledDiv = styled.div`
   padding-block: 1rem;
 `;
 
+const StyledImage = styled(Image)`
+  width: 100%;
+  height: auto;
+  position: absolute;
+`;
+
 export default function HomePage() {
   return (
     <StyledDiv>
       <StyledHeader>Hello from 🐬</StyledHeader>
+      <StyledImage alt="blue foamy water" src={Whirlpool} />
     </StyledDiv>
   );
 }
