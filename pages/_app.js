cb5f4b7fca4a5562ac59c3c892ae0adf3fe5585e
Mathis Santos <mail@mathissantos.com>
2024-01-19 10:31:21 +0100

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,19 @@ const StyledList = styled.ul`
   gap: 1rem;
 `;
 
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: var(--link-color);
+  padding: 0.5rem;
+  border-radius: 0.5rem;
+
+  &:hover {
+    color: var(--link-hover);
+    background-color: var(--background-color);
+    transition: 0.1s;
+  }
+`;
+
 export default function App({ Component, pageProps }) {
   return (
     <>
@@ -20,13 +33,13 @@ export default function App({ Component, pageProps }) {
       <StyledNav>
         <StyledList>
           <li>
-            <Link href="/">Home</Link>
+            <StyledLink href="/">Home</StyledLink>
           </li>
           <li>
-            <Link href="/about">About</Link>
+            <StyledLink href="/about">About</StyledLink>
           </li>
           <li>
-            <Link href="/contact">Contact</Link>
+            <StyledLink href="/contact">Contact</StyledLink>
           </li>
         </StyledList>
       </StyledNav>
--- styles.js
+++ styles.js
@@ -7,6 +7,12 @@ export default createGlobalStyle`
     box-sizing: border-box;
   }
 
+  :root {
+    --link-color: darkgreen;
+    --link-hover: blue;
+    --background-color: pink;
+  }
+
   body {
     margin: 0;
     font-family: system-ui;
